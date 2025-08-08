ee3b5ac373578ab31301461cf805ff82f69c6e18
Iram Lopez <iram644@hotmail.com>
2025-08-07 19:12:05 -0600

    basic cmake structure

---
+++ Step1_build/TutorialConfig.h
@@ -0,0 +1,4 @@
+#define Tutorial_VERSION_MINOR 0
+#define Tutorial_VERSION_MAJOR 1
+// the configured options and settings for Tutorial
+// TODO 10: Define Tutorial_VERSION_MAJOR and Tutorial_VERSION_MINOR
